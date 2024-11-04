872d55dd8d8658dad84d553c6778cf7cfb518d30
h14turkiye <h14turkiye@hotmail.com>
2024-11-04 17:15:20 +0300

    Add async scheduling in BlockBreakListener
    - Add inactivity-based reset for vein counts:
      - Track last interaction time for each player and reset counts if inactive beyond configured delay
      - Clean up player data on disconnect
    - Improve configuration handling for vein limits and permissions

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# XraySuspects
+# XraySuspect
 
-XraySuspects aims to integrate into Paper's anti-xray mechanisms, efficiently utilizing server resources to monitor and flag suspicious activities related to x-ray usage and only allowing Paper's antixray to be used against suspects.
+**XraySuspect** is designed to integrate with Paper's anti-xray mechanisms, effectively utilizing server resources to monitor and flag suspicious x-ray activities, while restricting Paper's anti-xray functionality to identified suspects.
 
 ## Warning
 
-While this plugin *may* optimize resource usage, please note that the performance impact of AntiXray modules or this plugin can be significant. The most effective anti-xray measures often involve discouraging players from using such cheats rather than solely relying on AntiXray.
+While this plugin *may* optimize resource usage, please note that it is not guaranteed(Complicated stuff about `use-permission`). Paper's AntiXray can increase network usage, and performance impacts from AntiXray modules, such as [Stonar's RayTraceAntiXray](https://github.com/stonar96/RayTraceAntiXray), can be significant. The most effective anti-xray strategies often focus on discouraging players from using cheats rather than relying solely on AntiXray measures.
 
-Be aware that exposing ores to users can potentially allow them to exploit seed anti-xray strategies, even if anti-xray features are enabled later. It is recommended to implement additional protection measures against such exploits.
+Be cautious, as exposing ores to players might enable them to exploit seed anti-xray strategies, even if anti-xray features are later enabled. It is advisable to implement additional protective measures against such exploits.
 
 ## Installation
 
 1. Ensure that the permission for world anti-xray configuration is enabled.
-2. Download the plugin and place it in the `plugins` folder of your Paper server.
-3. Add the Paper anti-xray bypass permission to your default group to enable functionality.
-4. Configure the plugin according to your server's requirements.
+2. Download the plugin and place it in your Paper server's `plugins` folder.
+3. Add the Paper anti-xray bypass permission to your default group to activate functionality.
+4. Configure the plugin according to your server's specific needs.
 
 ## Contributing
 
@@ -21,5 +21,4 @@ Contributions are welcome! If you have suggestions, improvements, or bug fixes, 
 
 ## TODO
 
-Asynchronous scheduling
-Folia support
+1. Create a good default configuration.
--- src/main/java/com/h14turkiye/xraysuspect/listener/BlockBreakListener.java
+++ src/main/java/com/h14turkiye/xraysuspect/listener/BlockBreakListener.java
@@ -16,77 +16,96 @@ import com.h14turkiye.xraysuspect.packet.PacketManager;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class BlockBreakListener implements Listener {
     
     private final Plugin plugin;
-    private final Map<Player, Location> playerLastLocation = new HashMap<>();
-    private final Map<Player, Map<String, Integer>> playerVeinCounts = new HashMap<>();
-    private static final int DISTANCE_SQUARED_THRESHOLD = 256; // Example: 16 blocks squared
+    private final Map<Player, Location> playerLastLocation = new ConcurrentHashMap<>();
+    private final Map<Player, Map<String, Integer>> playerVeinCounts = new ConcurrentHashMap<>();
+    private final Map<Player, Long> lastInteractionTimes = new ConcurrentHashMap<>();
+    private static final int DISTANCE_SQUARED_THRESHOLD = 256; // 16 blocks squared
+    
+    private final Set<String> veinKeys;
+    private final String giveCommand;
+    private final String revokeCommand;
+    private final int giveDelay;
+    private final boolean refreshChunks;
+    private final int resetCountInactivityDelay;
     
     public BlockBreakListener(Plugin plugin) {
         this.plugin = plugin;
+        
+        // Caching configuration values
+        this.veinKeys = plugin.getConfig().getConfigurationSection("veins").getKeys(false);
+        this.giveCommand = plugin.getConfig().getString("permission.command.give");
+        this.revokeCommand = plugin.getConfig().getString("permission.command.revoke");
+        this.giveDelay = plugin.getConfig().getInt("suspect-phase-time");
+        this.refreshChunks = plugin.getConfig().getBoolean("refresh-chunks");
+        this.resetCountInactivityDelay = plugin.getConfig().getInt("reset-count-inactivity-delay", 600);
+        
+        startInactivityResetTask();
     }
     
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        if(player.hasPermission("paper.antixray.bypass")) return;
-        Location currentLocation = event.getBlock().getLocation();
-        String blockTypeName = event.getBlock().getType().name().toLowerCase();
-        
-        // Get the "veins" configuration section from the plugin's config
-        Set<String> veinKeys = plugin.getConfig().getConfigurationSection("veins").getKeys(false); // e.g. diamond_ore: 2, gold_ore: 1
-        
-        // Check if the block broken is a recognized vein type
-        if (veinKeys.contains(blockTypeName)) {
-            // Initialize player's vein count if not already done
-            playerVeinCounts.putIfAbsent(player, new HashMap<>());
-            Map<String, Integer> veinCounts = playerVeinCounts.get(player);
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, ()-> {
+            Player player = event.getPlayer();
+            if (player.hasPermission("paper.antixray.bypass")) return;
             
-            // Update vein count for the specific block type
-            veinCounts.put(blockTypeName, veinCounts.getOrDefault(blockTypeName, 0) + 1);
+            Location currentLocation = event.getBlock().getLocation();
+            String blockTypeName = event.getBlock().getType().name().toLowerCase();
             
-            // Check if the player has moved significantly
-            Location lastLocation = playerLastLocation.get(player);
-            boolean movedSignificantly = lastLocation == null || hasMovedSignificantly(lastLocation, currentLocation);
-            
-            if (movedSignificantly) {
-                // Update the player's last known location
-                playerLastLocation.put(player, currentLocation);
+            // Check if the block broken is a recognized vein type
+            if (veinKeys.contains(blockTypeName)) {
+                // Initialize player's vein count if not already done
+                playerVeinCounts.putIfAbsent(player, new HashMap<>());
+                Map<String, Integer> veinCounts = playerVeinCounts.get(player);
                 
+                // Update last interaction time
+                lastInteractionTimes.put(player, System.currentTimeMillis());
                 
+                // Check if the player has moved significantly
+                Location lastLocation = playerLastLocation.get(player);
+                boolean movedSignificantly = lastLocation == null || hasMovedSignificantly(lastLocation, currentLocation);
                 
-                // Check if the vein limit for this material is exceeded
-                int veinLimit = plugin.getConfig().getInt("veins." + blockTypeName);
-                if (veinCounts.get(blockTypeName) > veinLimit) {
-                    String command = plugin.getConfig().getString("permission.revoke_command").replace("%player", player.getName()); // we revoke bypass permission
-                    if (command != null && !command.isEmpty()) {
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+                if (movedSignificantly) { 
+                    // Update vein count for the specific block type
+                    veinCounts.put(blockTypeName, veinCounts.getOrDefault(blockTypeName, 0) + 1);
+                    
+                    playerLastLocation.put(player, currentLocation);
+                    
+                    int veinLimit = plugin.getConfig().getInt("veins." + blockTypeName);
+                    if (veinCounts.get(blockTypeName) > veinLimit) {
+                        executeCommand(revokeCommand, player);
+                        
+                        // Schedule permission reapplication
+                        if (giveDelay > 0) {
+                            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                                if (player.isOnline()) {
+                                    executeCommand(giveCommand, player);
+                                }
+                            }, giveDelay);
+                        }
+                        
+                        if (refreshChunks) {
+                            resendNearbyChunks(player);
+                        }
                     }
-                    
-                    // we give the bypass perm again
-                    int delay = plugin.getConfig().getInt("permission.give_delay_ticks");
-                    if (delay > 0) {
-                        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-                            if(!player.isOnline()) return; 
-                            String command2 = plugin.getConfig().getString("permission.give_command").replace("%player", player.getName()); // we give bypass permission
-                            if (command2 != null && !command2.isEmpty()) {
-                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command2);
-                            }
-                        }, delay);
-                    }
-                    
-                    
-                    if(plugin.getConfig().getBoolean("refresh-chunks"))
-                    resendNearbyChunks(player);
                 }
             }
-        }
+        });
     }
     
     private boolean hasMovedSignificantly(Location lastLocation, Location currentLocation) {
         return (lastLocation.distanceSquared(currentLocation) > DISTANCE_SQUARED_THRESHOLD);
+    }
+    
+    private void executeCommand(String commandTemplate, Player player) {
+        if (commandTemplate != null && !commandTemplate.isEmpty()) {
+            String command = commandTemplate.replace("%player", player.getName());
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+        }
     }
     
     private void resendNearbyChunks(Player player) {
@@ -99,14 +118,31 @@ public class BlockBreakListener implements Listener {
         }
     }
     
+    private void startInactivityResetTask() {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, this::resetInactivePlayers, 0L, 20L * 60); // Run every minute
+    }
+    
+    private void resetInactivePlayers() {
+        long currentTime = System.currentTimeMillis();
+        
+        for (Map.Entry<Player, Long> entry : lastInteractionTimes.entrySet()) {
+            Player player = entry.getKey();
+            long lastInteractionTime = entry.getValue();
+            
+            if (currentTime - lastInteractionTime >= resetCountInactivityDelay * 50L) { // delay in ticks to milliseconds
+                playerVeinCounts.remove(player);
+                playerLastLocation.remove(player);
+                lastInteractionTimes.remove(player);
+            }
+        }
+    }
+    
     @EventHandler
     public void onPlayerDisconnect(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         playerLastLocation.remove(player);
         playerVeinCounts.remove(player);
-        String command = plugin.getConfig().getString("permission.give_command").replace("%player", player.getName()); // we give bypass permission
-        if (command != null && !command.isEmpty()) {
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
-        }
+        lastInteractionTimes.remove(player);
+        executeCommand(giveCommand, player);
     }
 }
